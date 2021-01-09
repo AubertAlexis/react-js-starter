8f1645b116d74ff851a5c034e32541e34ddbf7a2
Alexis Aubert <alexisaubert8109@outlook.fr>
2021-01-09 18:26:14 +0100

    Add scrollToTop

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './resources/style/app.scss';
+import ScrollToTop from './components/ScrollToTop';
 
 require("./resources/js/script.js");
 
 const App = () => {
+
+  <ScrollToTop />
 
   return (
     <Router>
